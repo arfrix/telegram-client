b56c636c20b60d4a763c7491ccef38a5a3546e66
arfa <arfrix.dev@gmail.com>
2021-01-26 10:44:59 +0330

    contact reducer updated

--- src/stores/contact/ContactReducer.js
+++ src/stores/contact/ContactReducer.js
@@ -1,13 +1,31 @@
 export function contactReducer(state , action){
     console.log(state)
     switch (action.type) {
-        case 'GET_CONTACTS':
+        case 'GET_CONTACTS_REQUEST':
             return{
                 ...state ,
                 contacts:{
-                    status : 200 ,
+                    status : 'LOADING' ,
+                    data : null ,
+                    error : null        
+                }
+            }
+        case 'GET_CONTACTS_SUCCESSFUL':
+            return{
+                ...state ,
+                contacts:{
+                    status : 'FINICHED' ,
                     data : action.payload ,
                     error : null        
+                }
+            }
+        case 'GET_CONTACTS_FAILURE':
+            return{
+                ...state ,
+                contacts:{
+                    status : 'FAILED' ,
+                    data : null ,
+                    error : action.payload        
                 }
             }
 
