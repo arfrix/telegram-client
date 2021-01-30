fc2a5f3390d1e6fac20470d927d6e31187455052
arfa <arfrix.dev@gmail.com>
2021-01-30 18:04:01 +0330

    contact ui improved

--- src/view/pages/contactInfo/ContactInfo.js
+++ src/view/pages/contactInfo/ContactInfo.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './ContactInfo.scss'
 
 export default function ContactInfo({data}) {
     console.log(data)
     return (
         <div className="contact">
-            {/* <img className="contact__profileImg" src={data.profilePic} /> */}
+            <img className="contact__profileImg" src={data.profilePic} />
             <div className="contact__name">{data.name}</div>
             <div className="contact__phoneNumber">{data.phoneNumber}</div>
         </div>
