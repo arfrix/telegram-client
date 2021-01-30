751397ae798a4bd52b3ff205bc3f6d70cd02d763
arfa <arfrix.dev@gmail.com>
2021-01-30 11:02:22 +0330

    last message added to list

--- src/view/components/list/List.js
+++ src/view/components/list/List.js
@@ -3,7 +3,7 @@ import images from '../../../res/images'
 import './List.scss'
 
 export default function List({itemsInfo, currentPage, onClick}) {
-  
+  console.log(itemsInfo)
 
     const isChat = currentPage === 'chat'
     
@@ -27,7 +27,7 @@ export default function List({itemsInfo, currentPage, onClick}) {
             <div className="card" onClick={() => onClick(data)}>
                 <img className="card__proifleImg" src={images.image.profilePicSample.default}/>
                 <div className="card__content">
-                    <CardContent username={data.name} lastMessage={data.messages[data.messages.length - 1].value} />
+                    <CardContent username={data.name} lastMessage={data.messages[data.messages.length - 1].message} />
                 </div>
             </div>
             
